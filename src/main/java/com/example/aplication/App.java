43995becb7199d2e5b21a0075239e0aa71183194
Slipvic <Slipvic667@gmail.com>
2023-07-03 16:52:16 -0300

    Classe Posição criada

---
+++ src/main/java/com/example/aplication/App.java
@@ -0,0 +1,16 @@
+package com.example.aplication;
+
+import com.example.tabuleiro.Position;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
