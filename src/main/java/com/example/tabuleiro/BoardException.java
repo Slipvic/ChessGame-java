087c567443d5b16c5595c69b567a2fe3b7053f83
Slipvic <Slipvic667@gmail.com>
2023-07-04 15:05:55 -0300

    Exceções e programação defensiva

---
+++ src/main/java/com/example/tabuleiro/BoardException.java
@@ -0,0 +1,11 @@
+package com.example.tabuleiro;
+
+public class BoardException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+
+}
