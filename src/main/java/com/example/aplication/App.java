55a7488978da7675221184b8a333f3d98907ac11
Slipvic <Slipvic667@gmail.com>
2023-07-05 14:28:49 -0300

    Melhorias na UI e Exceptions

--- src/main/java/com/example/aplication/App.java
+++ src/main/java/com/example/aplication/App.java
@@ -1,28 +1,39 @@
 package com.example.aplication;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.example.xadrez.ChessException;
 import com.example.xadrez.ChessMatch;
 import com.example.xadrez.ChessPiece;
 import com.example.xadrez.ChessPosition;
 
 public class App {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
+
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Origem: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Destino: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				UI.cleanScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Origem: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.print("Destino: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
-    }
+}
